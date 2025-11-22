237349ef1fd64070516b6153b3f3214fcea5f857
Juan Esteban Ruiz <jruizz@unal.edu.co>
2025-11-22 11:33:25 -0500

    Correcciones para correr el test

--- homework/src/_internals/read_all_lines.py
+++ homework/src/_internals/read_all_lines.py
@@ -1,7 +1,7 @@
 import os
 
 
-def read_all_lines(input_folder="data/input/"):
+def read_all_lines(input_folder):
     all_lines = []
     input_files_list = os.listdir(input_folder)
     for filename in input_files_list:
